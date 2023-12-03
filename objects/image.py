78a58e3d056b8444818445a56766862cccc3d9e5
Sukovatiy Kirill <kirillcykovatiy@gmail.com>
2023-12-03 20:19:40 +0300

    1

--- objects/image.py
+++
@@ -1,8 +0,0 @@
-from objects.base import BaseObject
-
-
-class Image(BaseObject):
-    def __init__(self, x, y, path, size):
-        super().__init__(x, y)
-        self.path = path
-        self.size = size
