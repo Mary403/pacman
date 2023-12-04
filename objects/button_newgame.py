bc853e7ddf0046e85029ed67dc7cc9eee9c44196
Беляк Мария <belakmasa@gmail.com>
2023-12-04 15:05:25 +0300

    Работает кнопка NewGame

---
+++ objects/button_newgame.py
@@ -0,0 +1,17 @@
+import sys
+
+from objects.button import Button
+from settings import Settings
+
+"""
+Класс кнопки новой игры
+"""
+
+
+class ButtonNewGame(Button):
+    def __init__(self, x, y, width, height, color=None, outline=False, text=None, color_text=None):
+        super().__init__(x, y, width, height, color, outline, text, color_text)
+
+    def click(self):  # событие клика
+        super().click()
+        Settings.set_scene(2)
--- scenes/menu.py
+++ scenes/menu.py
@@ -9,13 +9,14 @@ from raylib import colors
 
 from objects.button import Button
 from objects.button_exit import ButtonExit
+from objects.button_newgame import ButtonNewGame
 
 
 class MenuScene(BaseScene):  # Сцена 0
     def __init__(self):
         self.hello_text = Text(Settings.WIDTH // 2 - 90, Settings.HEIGHT // 2 - 200, "Welcome", 50,
                                (255, 255, 255, 255))
-        self.button1 = Button(Settings.WIDTH // 2 - 1000, Settings.HEIGHT // 2 - 50, 2000, 100,
+        self.button1 = ButtonNewGame(Settings.WIDTH // 2 - 1000, Settings.HEIGHT // 2 - 50, 2000, 100,
                               (20, 100, 20, 255), False, 'New game', (0, 255, 0, 255))
         self.button2 = ButtonExit(Settings.WIDTH // 2 - 1000, Settings.HEIGHT // 2 - 50 + 150, 2000, 100,
                                   (100, 20, 20, 255), False, 'Exit', (255, 0, 0, 255))
