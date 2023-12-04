9fddc0878504203ec3f354039b971a306632cd4c
Беляк Мария <belakmasa@gmail.com>
2023-12-04 15:17:03 +0300

    Кнопка настроек

---
+++ objects/button_settings.py
@@ -0,0 +1,15 @@
+from objects.button import Button
+from settings import Settings
+
+"""
+Класс кнопки настроек
+"""
+
+
+class ButtonSettings(Button):
+    def __init__(self, x, y, width, height, color=None, outline=False, text=None, color_text=None):
+        super().__init__(x, y, width, height, color, outline, text, color_text)
+
+    def click(self):  # событие клика
+        super().click()
+        Settings.set_scene(3)
--- scenes/menu.py
+++ scenes/menu.py
@@ -10,15 +10,18 @@ from raylib import colors
 from objects.button import Button
 from objects.button_exit import ButtonExit
 from objects.button_newgame import ButtonNewGame
+from objects.button_settings import ButtonSettings
 
 
 class MenuScene(BaseScene):  # Сцена 0
     def __init__(self):
-        self.hello_text = Text(Settings.WIDTH // 2 - 90, Settings.HEIGHT // 2 - 200, "Welcome", 50,
+        self.hello_text = Text(Settings.WIDTH // 2 - 90, Settings.HEIGHT // 2 - 200, "Pacman", 50,
                                (255, 255, 255, 255))
         self.button1 = ButtonNewGame(Settings.WIDTH // 2 - 1000, Settings.HEIGHT // 2 - 50, 2000, 100,
-                              (20, 100, 20, 255), False, 'New game', (0, 255, 0, 255))
-        self.button2 = ButtonExit(Settings.WIDTH // 2 - 1000, Settings.HEIGHT // 2 - 50 + 150, 2000, 100,
+                                     (20, 100, 20, 255), False, 'New game', (0, 255, 0, 255))
+        self.button2 = ButtonSettings(Settings.WIDTH // 2 - 1000, Settings.HEIGHT // 2 - 50 + 150, 2000, 100,
+                                      (20, 20, 100, 255), False, 'Settings', (0, 0, 255, 255))
+        self.button3 = ButtonExit(Settings.WIDTH // 2 - 1000, Settings.HEIGHT // 2 - 50 + 150*2, 2000, 100,
                                   (100, 20, 20, 255), False, 'Exit', (255, 0, 0, 255))
         super().__init__()
 
@@ -26,6 +29,7 @@ class MenuScene(BaseScene):  # Сцена 0
         self.objects.append(self.hello_text)
         self.objects.append(self.button1)
         self.objects.append(self.button2)
+        self.objects.append(self.button3)
 
     def additional_process_event(self):
         if pyray.is_key_pressed(pyray.KeyboardKey.KEY_ONE):
