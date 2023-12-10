1c94be8a064371716d95bdeff07d4a1c81fe7342
Беляк Мария <belakmasa@gmail.com>
2023-12-10 15:33:01 +0300

    Изи пакмен без призраков

--- scenes/game.py
+++ scenes/game.py
@@ -27,8 +27,8 @@ class GameScene(BaseScene):  # Сцена 2
         self.field_drawer = FieldDrawer(self.pole.data)
         self.seeds = SeedsLogic()
 
-        self.gameover_text = Text(Settings.WIDTH // 2 - 270, Settings.HEIGHT // 2 - 50, "GAME OVER", 100,
-                                  colors.RED)
+        self.gameover_text = Text(Settings.WIDTH // 2 - 230, Settings.HEIGHT // 2 - 50, "EASY WIN!", 100,
+                                  (10, 200, 50, 255))
         self.rec_gameover = Rect((Settings.WIDTH - 17 * 30) // 2, (Settings.HEIGHT - 21 * 30) // 2,
                                  Settings.WIDTH - (Settings.WIDTH - 17 * 30),
                                  Settings.HEIGHT - (Settings.HEIGHT - 21 * 30),
@@ -56,10 +56,8 @@ class GameScene(BaseScene):  # Сцена 2
 
         if self.rec_gameover in self.objects:
             self.objects.remove(self.rec_gameover)
-            print('удалился 1')
         if self.gameover_text in self.objects:
             self.objects.remove(self.gameover_text)
-            print('удалился 2')
         Settings.is_gameover = False
 
     def additional_process_event(self):
@@ -94,5 +92,4 @@ class GameScene(BaseScene):  # Сцена 2
     def gameover(self):
         self.objects.append(self.rec_gameover)
         self.objects.append(self.gameover_text)
-        print("конец")
         Settings.is_gameover = True
--- scenes/menu.py
+++ scenes/menu.py
@@ -14,11 +14,13 @@ class MenuScene(BaseScene):  # Сцена 0
     def __init__(self):
         self.hello_text = Text(Settings.WIDTH // 2 - 90, Settings.HEIGHT // 2 - 200, "Pacman", 50,
                                (255, 255, 255, 255))
+        self.hello_text2 = Text(Settings.WIDTH // 2 - 90, Settings.HEIGHT // 2 - 250, "Easy", 50,
+                                (10, 255, 50, 255))
         self.button1 = ButtonNewGame(Settings.WIDTH // 2 - 1000, Settings.HEIGHT // 2 - 50, 2000, 100,
                                      (20, 100, 20, 255), False, 'New game', (0, 255, 0, 255))
         self.button2 = ButtonSettings(Settings.WIDTH // 2 - 1000, Settings.HEIGHT // 2 - 50 + 150, 2000, 100,
                                       (20, 20, 100, 255), False, 'Settings', (100, 100, 255, 255))
-        self.button3 = ButtonExit(Settings.WIDTH // 2 - 1000, Settings.HEIGHT // 2 - 50 + 150*2, 2000, 100,
+        self.button3 = ButtonExit(Settings.WIDTH // 2 - 1000, Settings.HEIGHT // 2 - 50 + 150 * 2, 2000, 100,
                                   (100, 20, 20, 255), False, 'Exit', (255, 0, 0, 255))
 
         super().__init__()
@@ -28,6 +30,7 @@ class MenuScene(BaseScene):  # Сцена 0
         self.objects.append(self.button1)
         self.objects.append(self.button2)
         self.objects.append(self.button3)
+        self.objects.append(self.hello_text2)
 
     def additional_process_event(self):
         if pyray.is_key_pressed(pyray.KeyboardKey.KEY_ONE):
